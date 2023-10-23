291db78438664978242f073365bdd4303bc1edc0
Jenna Ray <jennastingray@gmail.com>
2023-10-23 16:35:42 -0600

    json

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,31 @@
 const baseURL = "https://jennaray.github.io/wdd230/";
 const linksURL = "https://jennaray.github.io/wdd230/data/links.json";
+const list = document.querySelector("#activities-list");
 
 async function getLinks(){
+    try {
+        
+    }
     const response = await fetch(linksURL);
     const data = await response.json();
     console.log(data);
+
 }
 
 getLinks();
+
+// function displayLinks(data){
+//     foreach (item in data.weeks){
+//         let w = document.createElement("li");
+//         let name = document.createElement("p");
+//         name.textContent = `${item.week} ||`;
+//         w.append(name);
+
+//         foreach (link in item.links){
+//             let l = document.createElement("a");
+//             l.setAttribute("href", `${link.url}`);
+//             l.innerHTML = `${link.title}`;
+//             w.appendChild(l);
+//         };
+//     };
+// }
